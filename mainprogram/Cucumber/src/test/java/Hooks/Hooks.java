3bf6cdb39877cf9099c9b0832bffb4b5efbf09f4
Pradeep956643 <pradeeprajendiren@gmail.com>
2019-11-12 14:52:52 +0530

    New method added

--- mainprogram/Cucumber/src/test/java/Hooks/Hooks.java
+++ mainprogram/Cucumber/src/test/java/Hooks/Hooks.java
@@ -1,5 +1,7 @@
 package Hooks;
 
+import org.junit.Test;
+
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
@@ -16,6 +18,12 @@ public class Hooks {
 		System.out.println("Close the Chrome");
 	}
 
+	@Test
+	public void newMethod() {
+
+		System.out.println("New Method has been added======");
+	}
+
 	/*
 	 * @Before("@First") public void beforeFirst() {
 	 * System.out.println("This will run only before the First Scenario"); }
@@ -30,23 +38,24 @@ public class Hooks {
 	 * 
 	 * @Before("@Four") public void beforeFour() {
 	 * System.out.println("This will run only before the fourth Scenario"); }
-	 
+	 * 
+	 * 
+	 * 
+	 * @After("@First") public void afterFirst() {
+	 * System.out.println("This will run only after the first scenario");
+	 * 
+	 * }
+	 * 
+	 * @After("@Second") public void aftersecond() {
+	 * System.out.println("This will run only after the second scenario"); }
+	 * 
+	 * @After("@Third") public void afterthird() {
+	 * 
+	 * System.out.println("This will run after the third scenarion"); }
+	 * 
+	 * @After("@Four") public void afterfour() {
+	 * 
+	 * System.out.println("This will run after the fourth scenario"); }
+	 */
 
-
-  @After("@First") public void afterFirst() {
-  System.out.println("This will run only after the first scenario");
-  
-  }
-  
-  @After("@Second") public void aftersecond() {
-  System.out.println("This will run only after the second scenario"); }
-  
-  @After("@Third") public void afterthird() {
-  
-  System.out.println("This will run after the third scenarion"); }
-  
-  @After("@Four") public void afterfour() {
-  
-  System.out.println("This will run after the fourth scenario"); }*/
-  
-  }
+}
